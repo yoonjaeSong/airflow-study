a72e1dcb540be263267b3f51f592bb024059072f
yoonjaeSong <thddbswo14@gmail.com>
2025-07-28 22:47:32 +0900

    shell script 추가

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,8 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+        echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+        echo "You selected Orange!"
+elif [ $FRUIT == PINEAPPLE ];then
+        echo "You selected Pineapple!"
+fi
