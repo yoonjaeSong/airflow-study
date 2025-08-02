b8055683cb6faf7428549ec4188282061210075e
yoonjaeSong <thddbswo14@gmail.com>
2025-08-02 11:31:32 +0900

    python 변수 사용 예시

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+import pendulum
+
+from plugins.common.common_func import regist
+
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,  # start_date에서 설정한 이전 값을 실행할지 안할지 결정하는 변수 True - 이전 값도 실행
+    tags=["example", "example2", "airflow-study"],
+) as dag:
+
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_args=["yoonjae song", "male", "kr", "경기도"]
+    )
+
+    regist_t1
