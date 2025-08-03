2fb0d47d2009b9eddb08a4591571fc73126ae973
yoonjaeSong <thddbswo14@gmail.com>
2025-08-03 18:18:25 +0900

    오타 수정

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -3,9 +3,6 @@ from airflow.providers.standard.operators.python import PythonOperator
 from airflow.decorators import task
 import pendulum
 
-from common.common_func import regist
-
-
 with DAG(
     dag_id="dags_python_template",
     schedule="30 9 * * *",
@@ -20,7 +17,7 @@ with DAG(
     python_t1 = PythonOperator(
         task_id='python_t1',
         python_callable=python_fuc1,
-        op_kwargs={'start_date': '{{date_interval_start | ds}}', 'end_date': '{{date_interval_end | ds}}'}
+        op_kwargs={'start_date': '{{data_interval_start | ds}}', 'end_date': '{{data_interval_end | ds}}'}
     )
 
     @task(task_id='python_t2')
