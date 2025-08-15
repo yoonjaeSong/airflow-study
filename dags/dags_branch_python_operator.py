dcf56f69d732cc952c80cd1f20b9b4b0779f7137
yoonjaeSong <thddbswo14@gmail.com>
2025-08-15 16:13:23 +0900

    Create dag exmaple using branch

---
+++ dags/dags_branch_python_operator.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+from airflow.providers.standard.operators.python import BranchPythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="dags_branch_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2025, 8, 1, tz="Asia/Seoul"),
+    catchup=False,  # start_date에서 설정한 이전 값을 실행할지 안할지 결정하는 변수 True - 이전 값도 실행
+    tags=["example", "example2", "airflow-study"],
+) as dag:
+
+    def select_random():
+        import random
+        
+        item_list = ['A', 'B', 'C']
+        selected_item = random.choice(item_list)
+        
+        if selected_item == 'A':
+            return 'task_a'
+        elif selected_item in ['B', 'C']:
+            return ['task_b', 'task_c']
+        
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
+    
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+        
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected': 'A'}
+    )
+    
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected': 'B'}
+    )
+    
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected': 'C'}
+    )
+    
+    python_branch_task >> [task_a, task_b, task_c]
