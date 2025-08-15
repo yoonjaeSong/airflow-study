fe13fbf68b80d965c2438fb3f7424d79b46c7d52
yoonjaeSong <thddbswo14@gmail.com>
2025-08-15 16:35:51 +0900

    task.branch annotation 사용하도록 변경

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -13,6 +13,7 @@ with DAG(
     tags=["example", "example2", "airflow-study"],
 ) as dag:
 
+    @task.branch(task_id='python_branch_task')
     def select_random():
         import random
         
@@ -23,11 +24,6 @@ with DAG(
             return 'task_a'
         elif selected_item in ['B', 'C']:
             return ['task_b', 'task_c']
-        
-    python_branch_task = BranchPythonOperator(
-        task_id='python_branch_task',
-        python_callable=select_random
-    )
     
     def common_func(**kwargs):
         print(kwargs['selected'])
@@ -50,4 +46,4 @@ with DAG(
         op_kwargs={'selected': 'C'}
     )
     
-    python_branch_task >> [task_a, task_b, task_c]
+    select_random() >> [task_a, task_b, task_c]
